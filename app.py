1d78f81c341b66a4b5ec6bf61b71f02fb8fe2e62
noxas69 <romain.dusson@gmail.com>
2022-01-09 12:51:43 +0100

    update

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import time
 import requests
 from bs4 import BeautifulSoup
 
-if sys.platform == "windows":
+
+if sys.platform == "win32":
     log_path = os.environ["USERPROFILE"]+"\\Saved Games\\Frontier Developments\\Elite Dangerous"
 else:
     log_path = "/var/log"
@@ -15,7 +16,6 @@ list_of_files = []
 for file in os.listdir(log_path):
     if file.endswith(".log"):
         list_of_files.append(os.path.join(log_path, file))
-print(list_of_files)
 latest_file = max(list_of_files, key=os.path.getctime)
 
 
@@ -23,19 +23,21 @@ def get_fsd_jump():
     last_jump = ""
 
     with open(latest_file, "r") as f:
-        line = reversed(f.readline())
+        line = f.readlines()
         for l in line:
             if "FSDJump" in l:
-                last_jump = line
+                last_jump = l
 
     if not last_jump == "":
         get_system(last_jump)
 
 
 def get_system(jump):
-    x = json.load(jump)
+    x = json.loads(jump)
     system = x["StarSystem"]
-    get_trade_raw("maia")
+    get_trade_raw(system)
+    get_trade_manu(system)
+    get_trade_data(system)
 
 
 def get_trade_raw(position):
@@ -45,20 +47,52 @@ def get_trade_raw(position):
     page = requests.get(url)
     soup = BeautifulSoup(page.text, "html.parser")
     for table in soup.findAll("tr"):
-        print(table.td[2].a.text)
+        if "pur" in table.getText():
+            for td in soup.findAll("td"):
+                if not td.strong == None:
+                    station = td.strong.getText()
+                    system = td.small.getText()
+                    break
+
+    print(f"Système : {system} | Station : {station}")
 
 
-def get_trade_manu():
+
+def get_trade_manu(position):
     station = ""
     system = ""
+    url = f"https://www.edsm.net/fr/search/stations/index/cmdrPosition/{position}/economy/5/service/71/sortBy/distanceCMDR"
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, "html.parser")
+    for table in soup.findAll("tr"):
+        if "Manufacturé" in table.getText():
+            for td in soup.findAll("td"):
+                if not td.strong == None:
+                    station = td.strong.getText()
+                    system = td.small.getText()
+                    break
+
+    print(f"Système : {system} | Station : {station}")
 
 
-def get_trade_data():
+def get_trade_data(position):
     station = ""
     system = ""
+    url = f"https://www.edsm.net/fr/search/stations/index/cmdrPosition/{position}/economy/4/service/71/sortBy/distanceCMDR"
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, "html.parser")
+    for table in soup.findAll("tr"):
+        if "Encodé" in table.getText():
+            for td in soup.findAll("td"):
+                if not td.strong == None:
+                    station = td.strong.getText()
+                    system = td.small.getText()
+                    break
+
+    print(f"Système : {system} | Station : {station}")
 
 
 while True:
-    time.sleep(2)
+    time.sleep(3)
     get_fsd_jump()
 
