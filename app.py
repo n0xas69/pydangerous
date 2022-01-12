d3621949c9b0d0218cbcb1fd27be03b0a0ff8fdd
n0xas <romain.dusson@gmail.com>
2022-01-12 20:54:00 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import os
 import sys
 import json
-import time
 import requests
+import time
 from bs4 import BeautifulSoup
 
 
@@ -35,22 +35,20 @@ def get_last_fsd_jump():
 
 
 def get_trade_raw():
-    #cmdr_position = get_last_fsd_jump()
-    cmdr_position = "maia"
+    cmdr_position = get_last_fsd_jump()
     station = ""
     system = ""
     url = f"https://www.edsm.net/fr/search/stations/index/cmdrPosition/{cmdr_position}/economy/3/service/71/sortBy/distanceCMDR"
     page = requests.get(url)
     soup = BeautifulSoup(page.text, "html.parser")
     for table in soup.findAll("tr"):
-        if "pur" in table.getText():
+        if "Pur" in table.getText():
             for td in soup.findAll("td"):
                 if td.strong is not None:
                     station = td.strong.getText()
                     system = td.small.getText()
                     break
 
-    print(f"Système : {system} | Station : {station}")
     return station, system
 
 
@@ -69,7 +67,6 @@ def get_trade_manu():
                     system = td.small.getText()
                     break
 
-    print(f"Système : {system} | Station : {station}")
     return station, system
 
 
@@ -88,6 +85,27 @@ def get_trade_data():
                     system = td.small.getText()
                     break
 
-    print(f"Système : {system} | Station : {station}")
     return station, system
 
+
+def clear():
+    if os.name == 'nt':
+        _ = os.system('cls')
+    else:
+        _ = os.system('clear')
+
+
+def get_information():
+    trade_raw = get_trade_raw()
+    trade_manu = get_trade_manu()
+    trade_data = get_trade_data()
+    return trade_raw, trade_manu, trade_data
+
+
+while True:
+    informations = get_information()
+    time.sleep(2)
+    clear()
+    print(f"Trader Brut     ---->   Station : {informations[0][0]} | Système :  {informations[0][1]} \n"
+          f"Trader Fabriqué ---->   Station : {informations[2][0]} | Système :  {informations[2][1]} \n"
+          f"Trader Data     ---->   Station : {informations[1][0]} | Système :  {informations[1][1]}")
