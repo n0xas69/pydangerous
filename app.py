6f07f8a5b067a6a8e7eaea2959ce7652e250f375
n0xas <romain.dusson@gmail.com>
2022-01-12 22:23:26 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ import requests
 import time
 from bs4 import BeautifulSoup
 
+# Le chemin par défaut du journal est déjà dans le code, vous pouvez le changer en renseignant cette variable
+LOG_PATH = ""
+
+if LOG_PATH:
+    log_path = LOG_PATH
 
 if sys.platform == "win32":
     log_path = os.environ["USERPROFILE"]+"\\Saved Games\\Frontier Developments\\Elite Dangerous"
@@ -12,30 +17,36 @@ else:
     log_path = "/var/log"
 
 # On trouve le dernier fichier de log
-list_of_files = []
-for file in os.listdir(log_path):
-    if file.endswith(".log"):
-        list_of_files.append(os.path.join(log_path, file))
-latest_file = max(list_of_files, key=os.path.getctime)
+try:
+    list_of_files = []
+    for file in os.listdir(log_path):
+        if file.endswith(".log"):
+            list_of_files.append(os.path.join(log_path, file))
+    latest_file = max(list_of_files, key=os.path.getctime)
+except Exception as e:
+    print("Impossible de trouver les fichiers journaux.")
 
 
 def get_last_fsd_jump():
     last_jump = ""
 
-    with open(latest_file, "r") as f:
-        line = f.readlines()
-        for l in line:
-            if "FSDJump" in l:
-                last_jump = l
+    try:
+        with open(latest_file, "r") as f:
+            line = f.readlines()
+            for l in line:
+                if "FSDJump" in l:
+                    last_jump = l
 
-    if last_jump is not None:
-        x = json.loads(last_jump)
-        system = x["StarSystem"]
-        return system
+        if last_jump is not None:
+            x = json.loads(last_jump)
+            system = x["StarSystem"]
+            return system
+
+    except Exception as e:
+        print("Impossible d'ouvrir le fichier journal du jeu.")
 
 
-def get_trade_raw():
-    cmdr_position = get_last_fsd_jump()
+def get_trade_raw(cmdr_position):
     station = ""
     system = ""
     url = f"https://www.edsm.net/fr/search/stations/index/cmdrPosition/{cmdr_position}/economy/3/service/71/sortBy/distanceCMDR"
@@ -52,8 +63,7 @@ def get_trade_raw():
     return station, system
 
 
-def get_trade_manu():
-    cmdr_position = get_last_fsd_jump()
+def get_trade_manu(cmdr_position):
     station = ""
     system = ""
     url = f"https://www.edsm.net/fr/search/stations/index/cmdrPosition/{cmdr_position}/economy/5/service/71/sortBy/distanceCMDR"
@@ -70,8 +80,7 @@ def get_trade_manu():
     return station, system
 
 
-def get_trade_data():
-    cmdr_position = get_last_fsd_jump()
+def get_trade_data(cmdr_position):
     station = ""
     system = ""
     url = f"https://www.edsm.net/fr/search/stations/index/cmdrPosition/{cmdr_position}/economy/4/service/71/sortBy/distanceCMDR"
@@ -96,16 +105,17 @@ def clear():
 
 
 def get_information():
-    trade_raw = get_trade_raw()
-    trade_manu = get_trade_manu()
-    trade_data = get_trade_data()
+    cmdr_position = get_last_fsd_jump()
+    trade_raw = get_trade_raw(cmdr_position)
+    trade_manu = get_trade_manu(cmdr_position)
+    trade_data = get_trade_data(cmdr_position)
     return trade_raw, trade_manu, trade_data
 
 
 while True:
+    clear()
     informations = get_information()
     time.sleep(2)
-    clear()
     print(f"Trader Brut     ---->   Station : {informations[0][0]} | Système :  {informations[0][1]} \n"
           f"Trader Fabriqué ---->   Station : {informations[2][0]} | Système :  {informations[2][1]} \n"
           f"Trader Data     ---->   Station : {informations[1][0]} | Système :  {informations[1][1]}")
